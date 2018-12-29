71b5b512d39adc881b0866606c5803873556367d
Edmundo rubio <edmundo@webaholics.co>
2018-12-29 16:42:31 -0700

    BareBones App

---
+++ src/ToDoApp/01-Pages/00-Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class MainComp extends React.Component {
+  render(){
+    return(
+      <div>
+        <h1>Main Component here</h1>
+        <p>More...</p>
+      </div>
+    )
+  }
+}
+
+export default MainComp
